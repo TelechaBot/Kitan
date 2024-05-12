d990952469efca1c10dd9e9c6a1999d044187e85
sudoskys <coldlando@hotmail.com>
2024-05-12 22:22:55 +0800

    ✨ feat(utils): add signature generation function
    Add signature generation function with HMAC encryption for message verification.

---
+++ backend/utils/signature.py
@@ -0,0 +1,23 @@
+import hashlib
+import hmac
+
+from pydantic import SecretStr
+
+
+def generate_sign(
+        message_id: str,
+        chat_id: str,
+        user_id: str,
+        join_time: str,
+        secret_key: SecretStr
+) -> str:
+    sign_value = f"{message_id}={chat_id}={user_id}={join_time}"
+    secret_key = secret_key.get_secret_value().encode()
+    sign = hmac.new(secret_key, sign_value.encode(), hashlib.sha256).hexdigest()
+    return sign
+
+
+if __name__ == '__main__':
+    from pydantic import SecretStr
+
+    print(generate_sign(1, 2, 3, 4, SecretStr("test")))
