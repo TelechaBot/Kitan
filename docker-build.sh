bbb5743661db9a8d5dd46c87f4e5888a895a2e39
sudoskys <coldlando@hotmail.com>
2024-07-31 23:52:53 +0800

    :truck: chore(frontend): Update Dockerfile to use http-server for serving static files

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,14 @@
 # docker-build.sh
 # frontend: Vite
-export VITE_BACKEND_URL='/api'
+export VITE_BACKEND_URL='verify.dianas.cyou' # Backend Domain
 export VITE_CLOUDFLARE_SITE_KEY='0x4AAAAAAAxxxxx' # Cloudflare Turnstile
 
 # backend: FastAPI
-export SERVER_HOST='0.0.0.0'
-export SERVER_PORT='10100'
+export SERVER_HOST='0.0.0.0' # Host(DO NOT CHANGE)
+export SERVER_PORT='10100' # Expose Port
 export CORS_ORIGIN='*'
-export VERIFY_DOMAIN='verifyer.dianas.cyou'
-export TELEGRAM_BOT_TOKEN='65655103:xxxxxx'
+export VERIFY_DOMAIN='verifyer.dianas.cyou' # Frontend Domain
+export TELEGRAM_BOT_TOKEN='65655103:xxxxxx' # Telegram Bot Token
 export CLOUDFLARE_SECRET_KEY='0x4AAAAAxxxxx' # Cloudflare Turnstile
 
 # Nginx
