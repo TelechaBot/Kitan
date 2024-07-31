c88eca2a917121e2a3eeca744bd74b2bc87e1ca2
sudoskys <coldlando@hotmail.com>
2024-07-31 21:50:12 +0800

    🐛 fix: update environment variables in docker-build.sh
    - Update VITE_BACKEND_URL for frontend
    - Add CORS_ORIGIN for backend
    - Update NGINX_SERVER_NAME for Nginx

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,17 @@
 # docker-build.sh
-export VITE_CLOUDFLARE_SITE_KEY='0x4AAAAAAAxxxxx' # Cloudflare Turnstile
+# frontend: Vite
 export VITE_BACKEND_URL='/api'
+export VITE_CLOUDFLARE_SITE_KEY='0x4AAAAAAAxxxxx' # Cloudflare Turnstile
 
+# backend: FastAPI
 export SERVER_HOST='0.0.0.0'
 export SERVER_PORT='10100'
-export CLOUDFLARE_SECRET_KEY='0x4AAAAAxxxxx' # Cloudflare Turnstile
+export CORS_ORIGIN='*'
 export VERIFY_DOMAIN='verifyer.dianas.cyou'
 export TELEGRAM_BOT_TOKEN='65655103:xxxxxx'
+export CLOUDFLARE_SECRET_KEY='0x4AAAAAxxxxx' # Cloudflare Turnstile
 
+# Nginx
 export NGINX_SERVER_NAME='verifyer.dianas.cyou'
 export SSL_CERTIFICATE_PATH='/etc/nginx/ssl/fullchain.pem'
 export SSL_CERTIFICATE_KEY_PATH='/etc/nginx/ssl/privkey.pem'
