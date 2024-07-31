f8d10af5a8d83dd5e7899e4f7976657ec6769ed7
sudoskys <coldlando@hotmail.com>
2024-07-31 15:05:46 +0800

    :recycle: chore(docker-build.sh): update NGINX_SERVER_NAME and VERIFY_DOMAIN    to use 'verifyer.dianas.cyou' instead of 'yourdomain.com' and    'https://verifyer.dianas.cyou' respectively

--- docker-build.sh
+++ docker-build.sh
@@ -5,10 +5,10 @@ export VITE_BACKEND_URL='/api'
 export SERVER_HOST='0.0.0.0'
 export SERVER_PORT='10100'
 export CLOUDFLARE_SECRET_KEY='0x4AAAAA'
-export VERIFY_DOMAIN='https://verifyer.dianas.cyou'
+export VERIFY_DOMAIN='verifyer.dianas.cyou'
 export TELEGRAM_BOT_TOKEN='65655103:xxxxxx'
 
-export NGINX_SERVER_NAME='yourdomain.com'
+export NGINX_SERVER_NAME='verifyer.dianas.cyou'
 export SSL_CERTIFICATE_PATH='/etc/nginx/ssl/fullchain.pem'
 export SSL_CERTIFICATE_KEY_PATH='/etc/nginx/ssl/privkey.pem'
 
