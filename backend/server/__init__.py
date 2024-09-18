242041671d1b5849bafa6c5c29fb1da02d9b5031
sudoskys <coldlando@hotmail.com>
2024-09-18 12:56:17 +0800

    ```plaintext :zap: feat(server): enhance logging for user and captcha details
    - Add user ID logging
    - Improve source and Telegram data logging
    ```

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -160,8 +160,7 @@ async def verify_captcha(captcha_data: VerifyData):
             status_code=400,
             content={"status": EnumStatu.error.value, "message": "EXPIRED_REQUEST"}
         )
-    logger.info(f"[Source] {captcha_data.source}")
-    logger.info(f"[User] {captcha_data.acc}")
+    logger.info(f"[Captcha] {captcha_data}")
     logger.info(f"[Telegram] {web_app_data}")
     if not captcha_data.acc.get("verify_mode"):
         return JSONResponse(
